b7ce459536b6bef599d699e484a9b5e0e0348488
Tom Houlé <tom@tomhoule.com>
2021-05-08 12:52:17 +0200

    wip

--- migration-engine/connectors/sql-migration-connector/src/sql_schema_differ/differ_database.rs
+++ migration-engine/connectors/sql-migration-connector/src/sql_schema_differ/differ_database.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-
-use sql_schema_describer::SqlSchema;
+use std::collections::{BTreeMap, HashMap};
+use sql_schema_describer::{SqlSchema, walkers::{SqlSchemaExt, TableWalker}};
 use crate::pair::Pair;
 
 pub(crate) struct DifferDatabase<'a> {
@@ -9,6 +8,7 @@ pub(crate) struct DifferDatabase<'a> {
     created_tables: Vec<usize>,
     table_pairs: Vec<Pair<usize>>,
     dropped_tables: Vec<usize>,
+    column_names: BTreeMap<(&'a str, &'a str), Pair<Option<usize>>>,
 }
 
 impl<'a> DifferDatabase<'a> {
@@ -17,6 +17,7 @@ impl<'a> DifferDatabase<'a> {
             schemas.previous().tables.len(), schemas.next().tables.len()
         );
         let mut table_names = HashMap::with_capacity(table_names_count_lb);
+        let mut column_names = BTreeMap::new();
 
         // We are biased to created tables in the first pass, because migrations
         // tend to add rather than remove.
@@ -24,11 +25,17 @@ impl<'a> DifferDatabase<'a> {
         let mut table_pairs = Vec::new();
 
         for table in schemas.previous().table_walkers() {
-            table_names.insert(table.name(), Pair::new(Some(table.table_index()), None ));
+            let table_name = table.name();
+            table_names.insert(table_name, Pair::new(Some(table.table_index()), None ));
+
+            for column in table.columns() {
+                column_names.insert((table_name, column.name()), Pair::new(Some(column.column_index()), None));
+            }
         }
 
         for table in schemas.next().table_walkers() {
-            let entry = table_names.entry(table.name()).or_default();
+            let table_name = table.name();
+            let entry = table_names.entry(table_name).or_default();
             *entry.next_mut() = Some(table.table_index());
 
             match entry.previous() {
@@ -39,15 +46,28 @@ impl<'a> DifferDatabase<'a> {
                     created_tables.push(table.table_index())
                 }
             }
+
+            for column in table.columns() {
+                let entry = column_names.entry((table_name, column.name())).or_default();
+                *entry.next_mut() = Some(column.column_index());
+
+            }
         }
 
-        let dropped_tables = todo!("A clever way to computed dropped tables without walking the tables again");
+        let dropped_tables = table_names.values().filter(|t| t.next().is_none()).filter_map(|t| *t.previous()).collect();
 
-
-        DifferDatabase { schemas, table_names, created_tables, table_pairs, dropped_tables }
+        DifferDatabase { schemas, table_names, created_tables, table_pairs, dropped_tables, column_names }
     }
 
-    pub(crate) fn created_tables(&self) -> &[usize] {
-        &self.created_tables
+    pub(crate) fn created_tables(&self) -> impl Iterator<Item = TableWalker<'a>> + '_ {
+        self.created_tables.iter().map(move |idx| self.schemas.next().table_walker_at(*idx))
+    }
+
+    pub(crate) fn dropped_tables(&self) -> impl Iterator<Item = TableWalker<'a>> + '_ {
+        self.dropped_tables.iter().map(move |idx| self.schemas.next().table_walker_at(*idx))
+    }
+
+    pub(crate) fn table_pairs(&self) -> impl Iterator<Item = Pair<TableWalker<'a>>> + '_ {
+        self.table_pairs.iter().map(move |idxs| self.schemas.tables(idxs))
     }
 }
